67ef4e4345a343c7814e6f8fb9199661c73c67c4
shubham0419 <shubham.e21@nsut.ac.in>
2025-04-27 13:17:09 +0530

    blogs

--- blog/public/script.js
+++ blog/public/script.js
@@ -1,24 +1,56 @@
-const categorySelect = document.getElementById("category");
-const categories = [
-  { id: 1, name: "Technology" },
-  { id: 2, name: "Travel" },
-  { id: 3, name: "Food" },
-  { id: 4, name: "Lifestyle" },
-  { id: 5, name: "Business" },
-];
+// const categorySelect = document.getElementById("category");
+// const categories = [
+//   { id: 1, name: "Technology" },
+//   { id: 2, name: "Travel" },
+//   { id: 3, name: "Food" },
+//   { id: 4, name: "Lifestyle" },
+//   { id: 5, name: "Business" },
+// ];
 
-populateCategoryDropdown()
+// populateCategoryDropdown()
 
-function populateCategoryDropdown() {
+// function populateCategoryDropdown() {
   
-  while (categorySelect.options.length > 1) {
-    categorySelect.remove(1);
-  }
+//   while (categorySelect.options.length > 1) {
+//     categorySelect.remove(1);
+//   }
 
-  categories.forEach((category) => {
-    const option = document.createElement("option");
-    option.value = category.id;
-    option.textContent = category.name;
-    categorySelect.appendChild(option);
+//   categories.forEach((category) => {
+//     const option = document.createElement("option");
+//     option.value = category.id;
+//     option.textContent = category.name;
+//     categorySelect.appendChild(option);
+//   });
+// }
+
+
+const loginForm = document.getElementById("login-form");
+const email = document.getElementById("username");
+const password = document.getElementById("password");
+
+loginForm.addEventListener("submit",async (e)=>{
+  e.preventDefault();
+  const useremail = email.value;
+  const userPass = password.value;
+
+  const res = await axios.post("http://localhost:3000/user/login",{
+    email:useremail,
+    password:userPass
   });
-}
+  document.cookie = res.data.token;
+
+  axios.get('/http://localhost:3000/getallblogs`', {
+    headers: {
+      'Authorization': `Bearer ${token}` 
+    }
+  })
+  .then(response => {
+    // Handle successful response
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle error
+    console.error(error);
+  });
+  console.log(res.data.token);
+})
