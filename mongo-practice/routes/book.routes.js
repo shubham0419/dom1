281bfb0a41518c16a1f693dc02fd30cc91344ccd
shubham0419 <shubham.e21@nsut.ac.in>
2025-05-05 21:23:54 +0530

    books system

--- mongo-practice/routes/book.routes.js
+++ mongo-practice/routes/book.routes.js
@@ -17,5 +17,32 @@ router.post("/create",async(req,res)=>{
   res.status(200).json({message:"book created successfully",book})
 })
 
+router.get("/all",async(req,res)=>{
+  const allBooks = await Book.find();
+  res.status(200).json({allBooks});
+})
+
+router.get("/:bookId",async(req,res)=>{
+  const {bookId} = req.params;
+  // const book = await Book.findById(bookId)
+  const book = await Book.findOne({_id:bookId});
+  res.status(200).json({book});
+})
+
+router.put("/update/:id",async(req,res)=>{
+  const {id} = req.params;
+  const dataToUpdate = req.body;
+  // await Book.findByIdAndUpdate(id,dataToUpdate)
+  // await Book.findByIdAndUpdate(id,req.body`)
+  await Book.updateOne({_id:id},req.body);
+  res.status(200).json({message:"book updated successfully"});
+})
+
+router.delete("/delete/:id",async(req,res)=>{
+  const {id} = req.params;
+  await Book.findByIdAndDelete(id);
+  // await Book.deleteOne({_id:id})
+  res.status(200).json({message:"book deleted successfully"});
+})
 
 module.exports = router;
